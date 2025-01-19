464cf39d49deab5bca941dd7f4ec5eeb55df6e5c
dikraHon <72998202+dikraHon@users.noreply.github.com>
2025-01-19 23:41:34 +0300

    Update Main.kt
    nothing

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,4 @@
 package org.example
 
 fun main() {
-    print("Enter num: ")
-    numPalindrome(readln().toInt())
-}
-
-
-/* Напишите программу, которая проверяет,
-является ли введённое пользователем число палиндромом
-(читается одинаково слева направо и справа налево).
-Используйте цикл для разбиения числа и сравнения цифр. */
-
-fun numPalindrome(n: Int) {
-    return if(n.toString() == n.toString().reversed())
-        println("number is palindrome")
-    else
-        println("number is not palindrome")
 }
